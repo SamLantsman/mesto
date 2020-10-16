6db08bd3cf89d9ef56be2b3ade456b7edb83cb63
Sam Lantsman <samlantsman@gmail.com>
2020-10-17 01:06:42 +0300

    bug fixed

--- script/index.js
+++ script/index.js
@@ -1,26 +1,38 @@
-let buttonOpenPopupEditProfile = document.querySelector(".profile__edit-button");
-let popupEditProfile = document.querySelector(".popup__edit-profile");
-let buttonOpenPopupAddCard = document.querySelector(".profile__add-button");
-let popupAddCard = document.querySelector(".popup__add-card");
-let buttonClosePopupEditProfile = popupEditProfile.querySelector(".popup__close-image");
-let buttonClosePopupAddCard = popupAddCard.querySelector(".popup__close-image");
-let saveButtonEditProfile = popupEditProfile.querySelector(".popup__save-button");
-let saveButtonAddCard = popupAddCard.querySelector(".popup__save-button");
+const buttonOpenPopupEditProfile = document.querySelector(".profile__edit-button");
+const popupEditProfile = document.querySelector(".popup__edit-profile");
+const buttonOpenPopupAddCard = document.querySelector(".profile__add-button");
+const popupAddCard = document.querySelector(".popup__add-card");
+const buttonClosePopupEditProfile = popupEditProfile.querySelector(".popup__close-image");
+const buttonClosePopupAddCard = popupAddCard.querySelector(".popup__close-image");
+const saveButtonEditProfile = popupEditProfile.querySelector(".popup__save-button");
+const saveButtonAddCard = popupAddCard.querySelector(".popup__save-button");
+const oldName = document.querySelector(".profile__name");
+const oldJob = document.querySelector(".profile__job");
+const newName = document.querySelector(".popup__text-name");
+const newJob = document.querySelector(".popup__text-job");
+const place = document.querySelector(".popup__text-place")
+const link = document.querySelector(".popup__text-image");
+const closeImagePopup = document.querySelector(".image-popup__close-image");
+const fullImage = document.querySelector(".image-popup__image");
+const fullImageText = document.querySelector(".image-popup__caption");
+
 
 
 const gallery = document.querySelector(".gallery")
 const template = document.querySelector('.template');
 
-const openPopup = function(data) {
-    data.classList.add("popup_is-opened")
+const openPopup = function(popup) {
+    popup.classList.add("popup_is-opened")
 }
 
-const closePopup = function(data) {
-    data.classList.remove("popup_is-opened");
+const closePopup = function(popup) {
+    popup.classList.remove("popup_is-opened");
 }
 
 buttonOpenPopupEditProfile.addEventListener("click", () => {
     openPopup(popupEditProfile);
+    newName.value = oldName.textContent;
+    newJob.value = oldJob.textContent;
 });
 
 buttonOpenPopupAddCard.addEventListener("click", () => {
@@ -38,32 +50,24 @@ buttonClosePopupAddCard.addEventListener("click", () => {
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    let Input = document.querySelectorAll(".popup__text");
 
-    let name = Input[0].value;
-    let job = Input[1].value;
-
-    let newName = document.querySelector(".profile__name");
-    let newJob = document.querySelector(".profile__job");
-
-    newName.textContent = name;
-    newJob.textContent = job;
+    oldName.textContent = newName.value;
+    oldJob.textContent = newJob.value;
+    closePopup(popupEditProfile);
 }
 
-saveButtonEditProfile.addEventListener("click", formSubmitHandler);
+saveButtonEditProfile.addEventListener('click', formSubmitHandler);
 
 saveButtonAddCard.addEventListener("click", (evt) => {
     evt.preventDefault();
-    let input = popupAddCard.querySelectorAll(".popup__text");
 
-    let Name = input[0].value;
-    let Link = input[1].value;
-
-    let data = { name: Name, link: Link }
+    const data = { name: place.value, link: link.value }
 
     const card = getCards(data);
 
     gallery.prepend(card);
+
+    closePopup(popupAddCard);
 
 });
 
@@ -101,11 +105,13 @@ const renderCards = () => {
     gallery.prepend(...cards);
 };
 
-const Toggle = () => {
-    let popupIsOpened = document.querySelector(".image-popup");
+const toggle = () => {
+    const popupIsOpened = document.querySelector(".image-popup");
 
     popupIsOpened.classList.toggle("image-popup_is-opened");
 }
+
+closeImagePopup.addEventListener('click', toggle);
 
 const getCards = (data) => {
     const card = template.content.cloneNode(true);
@@ -113,32 +119,30 @@ const getCards = (data) => {
     card.querySelector('.card__heading').innerText = data.name;
     card.querySelector('.card__image').src = data.link;
 
-    let buttonLike = card.querySelector(".card__like");
+    const buttonLike = card.querySelector(".card__like");
 
     buttonLike.addEventListener('click', () => {
         buttonLike.classList.toggle('card__like_clicked');
     });
 
-    let buttonRemove = card.querySelector(".card__delete");
+    const buttonRemove = card.querySelector(".card__delete");
 
     buttonRemove.addEventListener('click', (event) => {
         event.target.closest('.card').remove();
     });
 
-    let openPopup = card.querySelector(".card__image");
-    let closePopup = document.querySelector(".image-popup__close-image");
+    const openPopup = card.querySelector(".card__image");
 
 
-    openPopup.addEventListener('click', Toggle);
-    closePopup.addEventListener('click', Toggle);
+    // openPopup.addEventListener('click', );
+
 
     openPopup.addEventListener('click', () => {
-
-        let fullImage = document.querySelector(".image-popup__image");
-        let fullImageText = document.querySelector(".image-popup__caption");
+        toggle();
 
         fullImage.src = openPopup.src;
         fullImageText.textContent = data.name;
+
     })
 
     return card;
